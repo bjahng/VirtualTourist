4845772c2b1f4f49636b9d7afd0efd7dfa6bbc9e
admin <admin@macbook-pro.local>
2018-02-12 10:34:17 -0800

    Initial commit with UI laid out.

---
+++ VirtualTourist/New Group/View/Controllers/AlbumViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by admin on 2/12/18.
+//  Copyright © 2018 DoughDoughTech. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import CoreData
+
+class AlbumViewController: UIViewController {
+    
+    
+    
+}
---
+++ VirtualTourist/New Group/View/Controllers/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  VirtualTourist
+//
+//  Created by admin on 2/8/18.
+//  Copyright © 2018 DoughDoughTech. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreData
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
